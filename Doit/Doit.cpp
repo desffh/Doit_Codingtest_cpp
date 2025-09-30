672645778b3cd2c68e04e7d6995f22122b1ca68f
desffh <yangju058@gmail.com>
2025-09-30 11:42:28 +0900

    (2025.09.30) 그리디2

--- Doit/Doit.cpp
+++ Doit/Doit.cpp
@@ -7,91 +7,47 @@
 using namespace std;
 
 int main()
-
 {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
 
 	/*
-		1744번 수를 묶어서 최댓값 만들기
+		1931번 회의실 배정하기
+
+		겹치지 않게 한 개의 회의실에서 할 수 있는 회의의 최대 개수는
+		회의시간이 짧은 순에서 겹치지 않게 짜면 된다.
 	*/
 
-	int N; // 수열의 크기
+	int N; // 회의의 개수
+	int result = 0;
+	int end = -1;
 
 	cin >> N;
 
-	priority_queue<int, vector<int>, less<int>> A; // 양수 저장 - 내림차순
-	priority_queue<int, vector<int>, greater<int>> B; // 음수 저장 - 오름차순
-	int one = 0; // 1의 개수
-	int zero = 0; // 0의 개수
-	int result = 0;
+	// 회의 정보 : 시작 시간, 끝 시간
+	vector <pair<int,int>> A;
+	
+	for (int i = 0; i < N; i++)
+	{
+		int start;
+		int end;
+		cin >> start >> end;
+
+		// 종료 시간 정렬을 위해 종료시간을 first로 저장
+		A.push_back(pair<int, int>(end, start));
+	}
+
+	sort(A.begin(), A.end());
 
 	for (int i = 0; i < N; i++)
 	{
-		int num;
-		cin >> num;
-
-		// 종류별로 입력받기
-
-		if (num == 0) // 0이라면
+		if (A[i].second >= end)
 		{
-			zero++;
-		}
-		else if (num == 1) // 1이라면
-		{
-			one++;
-		}
-		else if (num > 1) // 양수라면
-		{
-			A.push(num);
-		}
-		else if (num < 0) // 음수라면
-		{
-			B.push(num);
+			end = A[i].first;
+			result++;
 		}
 	}
-
-	// 양수
-	while (A.size() > 1)
-	{
-		int num1 = A.top();
-		A.pop();
-		int num2 = A.top();
-		A.pop();
-
-		result += num1 * num2;
-	}
-
-	// 양수에 데이터가 남아있다면 
-	if (A.size() >= 1)
-	{
-		result += A.top();
-	}
-
-	// 음수
-	while (B.size() > 1)
-	{
-		int num1 = B.top();
-		B.pop();
-		int num2 = B.top();
-		B.pop();
-
-		result += num1 * num2;
-	}
-
-	// 음수에 데이터가 남아있다면 
-	if (B.size() >= 1)
-	{
-		// 0이 하나도 없다면 그대로 더하기
-		if (zero == 0)
-		{
-			result += B.top();
-		}
-		// 0이 있다면 B.top() * 0 == 0 이기 때문에 아무것도 더하지 않는다.
-	}
-
-	result += one;
 
 	cout << result << '\n';
 }
